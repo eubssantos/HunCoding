424a340b13af0fc15aced07e2e9f60da0da9dbe9
Bruna Silva Santos <eubssantos@outlook.com>
2024-05-19 18:25:42 -0300

    first commit

--- src/main/main.go
+++ src/main/main.go
@@ -1,1 +1,23 @@
-package HunCoding
+package main
+
+import (
+	"HunCoding/src/main/route"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"log"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	router := gin.Default()
+
+	route.InitRoutes(&router.RouterGroup)
+
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal(err)
+	}
+}
--- src/main/route/routes.go
+++ src/main/route/routes.go
@@ -1,1 +1,15 @@
 package route
+
+import (
+	"HunCoding/src/main/controller"
+	"github.com/gin-gonic/gin"
+)
+
+func InitRoutes(r *gin.RouterGroup) {
+	//-------- User --------//
+	r.GET("/get-user-by-id/:userId", controller.FindUserById)
+	r.GET("/get-user-by-email/:email", controller.FindUserByEmail)
+	r.POST("/create-user", controller.CreateUser)
+	r.PUT("/update-ser/:userId", controller.UpdateUser)
+	r.DELETE("/delete-user/:userId", controller.DeleteUser)
+}
